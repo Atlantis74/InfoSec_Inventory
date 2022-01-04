5305ed5901171b90342d14dd82297b97b6170a90
Atlantis74 <88770882+Atlantis74@users.noreply.github.com>
2022-01-04 09:39:14 +0100

    Add files via upload

---
+++ scan.nmap.py
@@ -0,0 +1,32 @@
+url=""
+domain = ""
+capture_location="capture/"
+targets_file_all="targets_all.txt"
+targets_file_http="targets_http.txt"
+targets_file_https="targets_https.txt"
+targets_file_domains="targets_email.txt"
+
+# Parse file
+## Extract Domains from the list ALL
+
+## Extract subdomains vs IP addresses from the list ALL
+
+## List host that cannot be resolved from the list (subdomains)
+
+## List host not responding from the list (IP)
+
+## List hosts doing Emails stuffs (smtp, pop3, imap, ... )
+
+## List hosts doing SSH
+
+## List hosts telling it does telnet
+
+## List hosts doing FTP
+
+## List hosts doing Http Only
+
+## List hosts doing HTTPS 
+
+
+
+
